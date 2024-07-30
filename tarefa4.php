60d172ec92fd4ec44e28c52b6b81be332e29ff78
Salvatiico <fernandosalvatiko07@gmail.com>
2024-07-30 16:51:01 -0300

    Add files via upload

---
+++ tarefa4.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$a=3;
+echo "O valor atual de a é: " .$a ."<br/>";
+echo "O valor pré incrementado é: " .++$a . "<br/>";
+
+echo "O valor atual é: " .$a++ ."<br/>";
+echo "O valor pós incrementado é: ".$a;
+
+
+?>
